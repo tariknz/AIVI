73a4901e650e4d228dd0f70b1a64e37c3987f7ee
Tarik Alani <tarik.nzl@gmail.com>
2011-10-16 15:27:34 +0400

    logo resources

---
+++ logos/text_andreo.h
@@ -0,0 +1,24 @@
+const char *msg = 
+/*
+ * console is 40 cols x 30 lines
+ *
+ *        1234567890123456789012345678901234567890
+ */
+/* 01 */ "\n"
+/* 02 */ "\n"
+/* 03 */ "\n"
+/* 04 */ "\n"
+/* 05 */ "\n"
+/* 06 */ "Tarik Al-Ani\n"
+/* 07 */ "University of Otago 2010\n" 
+/* 08 */ "                                  88                                       \n"
+/* 09 */ "                                  88                                       \n"
+/* 10 */ "                                  88                                       \n"
+/* 11 */ ",adPPYYba,  8b,dPPYba,    ,adPPYb,88  8b,dPPYba,   ,adPPYba,   ,adPPYba,   \n"
+/* 12 */ "\"\"     `Y8  88P'   `\"8a  a8\"    `Y88  88P'   \"Y8  a8P_____88  a8\"     \"8a  \n"
+/* 13 */ ",adPPPPP88  88       88  8b       88  88          8PP\"\"\"\"\"\"\"  8b       d8  \n"
+/* 14 */ "88,    ,88  88       88  \"8a,   ,d88  88          \"8b,   ,aa  \"8a,   ,a8\"  \n"
+/* 15 */ "`\"8bbdP\"Y8  88       88   `\"8bbdP\"Y8  88           `\"Ybbd8\"'   `\"YbbdP\"'    Version 0.1.21"
+         ; /* End */
+
+		                                                                          
